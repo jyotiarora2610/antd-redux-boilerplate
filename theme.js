6392f451b7e851d7ebae983cdf3a772c811f0375
jyotiarora2610 <jyotiarora2610@gmail.com>
2018-04-24 16:33:25 +0530

    init commit

---
+++ theme.js
@@ -0,0 +1,23 @@
+// '@fill-body': #f1f6f8,
+module.exports = {
+  '@color-text-base': '#333333',
+  '@color-text-placeholder': '#aaaaaa',
+  '@color-text-disabled': 'rgba(0, 0, 0, 0.08)',
+  '@color-text-caption': '#5d5d5d',
+  '@color-text-paragraph': '#5d5d5d',
+  '@color-link': '#80c241',
+  '@color-shadow': '#000000',
+  '@fill-tap': '#ddd',
+  '@fill-disabled': '#ddd',
+  '@fill-mask': 'rgba(0, 0, 0, 0.65)',
+  '@fill-overlay-inverse': 'rgba(0, 0, 0, 0.7)',
+  '@primary-button-fill-tap': '#63a440',
+  '@brand-primary': '#80c241',
+  '@brand-primary-tap': '#8BC752',
+  '@brand-success': '#52cd4c',
+  '@brand-error': '#f35833',
+  '@brand-important': '#ff3b30',
+  '@toast-zindex': '9999999',
+  '@border-color-base': '#ddd'
+
+}
